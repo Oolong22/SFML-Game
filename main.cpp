3d3231d76cc68d7cda49b6ae32d5de4f871d1db5
Oolong22 <144280695+Oolong22@users.noreply.github.com>
2024-02-08 13:26:53 -0500

    blubl

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
-#include "SFML/Graphics.hpp"
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "cocka");
 
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -15,8 +12,28 @@ int main()
                 window.close();
         }
 
+        sf::Vertex vertex(sf::Vector2f(10.f, 50.f), sf::Color::Red, sf::Vector2f(100.f, 100.f));
+
+        // create an array of 3 vertices that define a triangle primitive
+        sf::VertexArray triangle(sf::Triangles, 3);
+
+        // define the position of the triangle's points
+        triangle[0].position = sf::Vector2f(250.f, 17.f);
+        triangle[1].position = sf::Vector2f(500.f, 45.f);
+        triangle[2].position = sf::Vector2f(500.f, 500.f);
+
+        // define the color of the triangle's points
+        triangle[0].color = sf::Color::Red;
+        triangle[1].color = sf::Color::Transparent;
+        triangle[2].color = sf::Color::Green;
+
+        // clear the window
         window.clear();
-        window.draw(shape);
+
+        // draw the triangle
+        window.draw(triangle);
+
+        // display the window content
         window.display();
     }
 
